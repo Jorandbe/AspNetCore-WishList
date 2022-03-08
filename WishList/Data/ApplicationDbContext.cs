d6960ee942987784b513d25a132961605b123db3
Brandon Jorandby <Jorandbe@gmail.com>
2022-03-07 22:47:34 -0600

    Module 4
    Create ApplicatoinDbContext
    Configured for EF
    Added Item Model

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        #region Properties
+
+        public DbSet<Item> Items { get; set; }
+
+        #endregion Properties
+
+        #region Constructors + Destructors
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        #endregion Constructors + Destructors
+    }
+}
